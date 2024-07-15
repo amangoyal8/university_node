093a51eb4daa0c095e1a61a4bb9d3a47574ae8b6
travelportal <amangoyal96940@gmail.com>
2024-07-15 16:50:11 +0530

    index

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,6 @@ const router = express.Router();
 const data = require('../controller/index');
 router.get('/getlist', data.handleGetFav);
 router.post('/setfav', data.handleSunbmitData);
-router.delete('/delfav', data.handleRemoveFav);
+router.delete('/delfav/:id', data.handleRemoveFav);
 
 module.exports = router;
